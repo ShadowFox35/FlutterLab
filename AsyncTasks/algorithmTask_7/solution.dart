ae0f88f18670600759d53b7649ff92c1ddfbcee6
ShadowFox35 <anastasia97chernova@gmail.com>
2023-06-06 18:23:21 +0200

    feat: readme and solution for task 7

---
+++ AsyncTasks/algorithmTask_7/solution.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+import 'package:dio/dio.dart';
+
+Future<void> getPokemonsList(offset, limit) async {
+  final dio = Dio();
+  final response = await dio
+      .get('https://pokeapi.co/api/v2/pokemon?offset=$offset&limit=$limit');
+
+  if (response.statusCode == 200) {
+    final pokemonList = response.data['results'];
+    final List<String> nameList = <String>[];
+
+    for (final pokemon in pokemonList) {
+      final String name = pokemon['name'];
+      nameList.add(name);
+    }
+
+    print(nameList);
+  } else {
+    print('похоже, что покемоны кончились');
+  }
+  return;
+}
+
+void main() async {
+  getPokemonsList(1, 5);
+}
