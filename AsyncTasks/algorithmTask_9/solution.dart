7b4c81db4fee6aa9000ff014beba2715f3052af5
ShadowFox35 <anastasia97chernova@gmail.com>
2023-06-07 13:53:19 +0200

    refactor: solution for task 9

--- AsyncTasks/algorithmTask_9/solution.dart
+++ AsyncTasks/algorithmTask_9/solution.dart
@@ -4,13 +4,13 @@ import 'dart:math';
 Stream<int> randomNumberGenerator(int min, int max) async* {
   final random = Random();
   while (true) {
-    yield min + random.nextInt(max - min + 1);
+    yield min + random.nextInt(max - min);
     await Future.delayed(Duration(seconds: 1));
   }
 }
 
 void main() {
-  final stream = randomNumberGenerator(1, 100);
+  final stream = randomNumberGenerator(50, 100);
 
   late StreamSubscription<int> subscription;
   subscription = stream.listen((number) {
